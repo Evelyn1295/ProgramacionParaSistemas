0c71f583984a0e20bdabf97f62a1fe989c3e660a
Gisel <39348352+Evelyn1295@users.noreply.github.com>
2018-09-28 13:35:15 +0200

    Update argumentos.c

--- argumentos.c
+++ argumentos.c
@@ -17,7 +17,6 @@ modificadores de printf()
     /*declaracion de variables */
     print("El número de argumentos es %d\n",argc);
     printf("Se le ha llamado con %s\n",argv[0]);
-    int tamanyo=0;
     for(i=1; i<argv;i++){
       printf("el argumento es %s\n",i,argv[i]);
     }
